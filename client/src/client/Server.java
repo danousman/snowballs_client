c8368522a105863ed80d96e66a648375999647f2
Danila <daniladashko@gmail.com>
2018-10-13 15:52:01 +0600

    Add basic functionality to connect to server.

---
+++ client/src/client/Server.java
@@ -0,0 +1,42 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Server {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    
+    public boolean startConnection(String host, int port) {
+        try {
+            this.clientSocket = new Socket(host, port);
+            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        this.out.println(msg);
+        return in.readLine();
+    }
+    
+    public boolean stopConnection() {
+        try {
+            this.clientSocket.shutdownInput();
+            this.clientSocket.shutdownOutput();
+            this.clientSocket.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
